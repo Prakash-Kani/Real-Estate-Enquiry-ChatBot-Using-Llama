7c7e2917bd101c055b0ea7bceadf46b3246ded2b
Prakash-Kani <prakashkoffi@gmail.com>
2024-10-14 19:29:08 +0530

    Updated model name

--- Real_Estate_Enquiry.py
+++ Real_Estate_Enquiry.py
@@ -10,7 +10,7 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
-model_name = 'gemma2:9b-instruct-q5_K_M''
+model_name = 'gemma2:9b-instruct-q5_K_M'
 embeddings_model_name =  "all-MiniLM-L6-v2"
 
 model = Ollama(model = model_name)
